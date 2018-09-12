875d7e0836d4d7f7cbbcfdf8c23ee06e6396fe17
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-11 21:17:22 -0500

    cleaned up html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log('Hello');
+
+/// Media - Image Gallery ///
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+};
+
+function showDivs(n) {
+  var newImages = document.getElementsByClassName("media-images");
+  if ( n > newImages.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = newImages.length}
+  for (i = 0; i < newImages.length; i++) {
+     newImages[i].style.display = "none";  
+  }
+  newImages[slideIndex-1].style.display = "block";  
+};
