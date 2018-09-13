f234906b8ceeaed7187c9dd43893dca440a8fa80
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-12 20:13:37 -0500

    created js function and print to DOM for album songs

--- js/main.js
+++ js/main.js
@@ -6,48 +6,52 @@ const printToDom = (stringToPrint, elementId) => {
 
 const ourAlbums = [
     {
-      name: 'My Boo',
-      songs: [{ id: 0, songTitle: "when I'm gone" }, { id: 1, songTitle: 'sugar' }, { id: 2, songTitle: 'tootsie' }]
+      name: 'The Secret',
+      songs: [{ id: 1, songTitle: "when I'm gone" }, { id: 2, songTitle: 'sugar' }, { id: 3, songTitle: 'tootsie' }]
     },
     {
-      name: 'Bingo',
-      songs: [{ id: 3, songTitle: 'hey baby' }, { id: 4, songTitle: 'kiss me twice' }, { id: 5, songTitle: 'cheating wife' }]
-    }
-  ];
-  
-  const ourBios = [
+      name: 'Gore',
+      songs: [{ id: 4, songTitle: 'hey baby' }, { id: 5, songTitle: 'kiss me twice' }, { id: 6, songTitle: 'fantazmo' }]
+    },
     {
-      name: 'Marco',
-      favSongs: [{ id: 1 }, { id: 3 }, { id: 5 }]
+      name: 'The Pink Album',
+      songs: [{ id: 7, songTitle: 'pumpkin spice' }, { id: 8, songTitle: 'red balloon' }, { id: 9, songTitle: 'puppies' }]
     }
   ];
   
-  function bandBioStringBuilder() {
-    let newString = '';
-    // Loop through the Bios
-    for (let i = 0; i < ourBios.length; i++) {
-      newString += `<h3>${ourBios[i].name}</h3>`;
-      newString += `<ul>`;
-      // Loop through the array of favorite songs
-      for (let s = 0; s < ourBios[i].favSongs.length; s++) {
-        // Loop through the Albums and do a find for the SongTitle that matches the Favorte Song ID in the Bio.
-        for (let album = 0; album < ourAlbums.length; album++) {
-          // Grab the songId from the Band memeber Bio
-          let favSongId = ourBios[i].favSongs[s].id;
-          let songFindResult = ourAlbums[album].songs.find((songId) => songId.id === favSongId);
-          // If we don't find a song match in the Album it returns undefind so just move along.
-          if (songFindResult !== undefined) {
-            // We found a match so grab the song title from the result and put it in a <li>
-            newString += `<li>${songFindResult.songTitle}</li>`;
-          }
-        }
-      }
-      // Close up the <ul>
-      newString += `</ul>`;
-    }
-    printToDom(newString, 'somHTMLElementHere');
-  }
-}
+//   const ourBios = [
+//     {
+//       name: 'Marco',
+//       favSongs: [{ id: 1 }, { id: 3 }, { id: 5 }]
+//     }
+//   ];
+  
+//   function bandBioStringBuilder() {
+//     let newString = '';
+//     // Loop through the Bios
+//     for (let i = 0; i < ourBios.length; i++) {
+//       newString += `<h3>${ourBios[i].name}</h3>`;
+//       newString += `<ul>`;
+//       // Loop through the array of favorite songs
+//       for (let s = 0; s < ourBios[i].favSongs.length; s++) {
+//         // Loop through the Albums and do a find for the SongTitle that matches the Favorte Song ID in the Bio.
+//         for (let album = 0; album < ourAlbums.length; album++) {
+//           // Grab the songId from the Band memeber Bio
+//           let favSongId = ourBios[i].favSongs[s].id;
+//           let songFindResult = ourAlbums[album].songs.find((songId) => songId.id === favSongId);
+//           // If we don't find a song match in the Album it returns undefind so just move along.
+//           if (songFindResult !== undefined) {
+//             // We found a match so grab the song title from the result and put it in a <li>
+//             newString += `<li>${songFindResult.songTitle}</li>`;
+//           }
+//         }
+//       }
+//       // Close up the <ul>
+//       newString += `</ul>`;
+//     }
+//     printToDom(newString, 'somHTMLElementHere');
+//   }
+// }
 
 
 /// Media - Image Gallery ///
@@ -70,3 +74,23 @@ function showDivs(n) {
 }
 
 /// End Media Image Gallery ///
+
+
+/// Media - Album Function ///
+
+const albumOne = () => {
+  let newString = ' ';
+  for(i = 0; i < ourAlbums.length; i++){
+    for(t=0; t < ourAlbums[i].songs.length; t++){
+    let songId = ourAlbums[i].songs[t].id;
+    let songTitle = ourAlbums[i].songs[t].songTitle;
+    newString += `<div class="song">`;
+    newString += `<h1>${songId}</h1>`;
+    newString += `<h1>${songTitle}</h1>`;
+    newString += `</div>`; 
+  }
+}
+  printToDom(newString,'songs');
+};
+
+albumOne ();
