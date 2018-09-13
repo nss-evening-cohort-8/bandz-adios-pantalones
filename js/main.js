4c801836be785a56dc36beb6e0f99b18bc013b01
Timothy Harley <timothy.d.harley@gmail.com>
2018-09-12 20:16:55 -0500

    finished js backbone

--- js/main.js
+++ js/main.js
@@ -31,43 +31,43 @@ let merch_data = {
         {
             name: 'Inspiration Bottle',
             picture: '../images/img_MERCH/bottle.jpg',
-            description: '',
-            price: '$5.99',
+            description: 'This bottle of inspiration might be empty, but it can filled with your hopes and dreams!  All your hopes and dreams can smell just like our inspiration!',
+            price: '₩5000 / $5.99',
             buy: 'BUY',
         },
         {
             name: 'Stained Pillow',
             picture: '../images/img_MERCH/pillow.jpg',
-            description: '',
-            price: '$599.99',
+            description: 'This pillow is stained with all the colors of our love.  Each pillow is unique to a band member and has been carefully prepared over several weeks.  Supplies are limited!',
+            price: '₩599000 / $599.99',
             buy: 'BUY',
         },
         {
             name: 'Flavor Infused Plate',
             picture: '../images/img_MERCH/plate.jpg',
-            description: '',
-            price: '59.99',
+            description: 'Paper plates are bad for the environment.  So we buy single use ceramic plates, and once we’re done, sell them to you at a huge discount!  Each plate is flavored to a recent meal of one of our bandmates.',
+            price: '₩59000 / $59.99',
             buy: 'BUY',
         },
         {
             name: 'Dulled Razor',
             picture: '../images/img_MERCH/razor.jpg',
-            description: '',
-            price: '16.99',
+            description: 'These are the actual razors the band uses to shave their legs!  Each razor represents months of daily shaving.  Great for children!',
+            price: '₩16000 / $16.99',
             buy: 'BUY',
         },
         {
             name: 'Bar Soap',
             picture: '../images/img_MERCH/soap.jpg',
-            description: '',
-            price: '29.99',
+            description: 'This was a puddle of liquid soap that sat too long.  We call it bar soap because we found it on the floor of a bar! Bonus: may contain unknown scents!',
+            price: '₩29000 / $29.99',
             buy: 'BUY',
         },
         {
             name: 'Community Toothbrush',
             picture: '../images/img_MERCH/tooth.jpg',
-            description: '',
-            price: '29.99',
+            description: 'We take out teeth hygiene very seriously.  So should you!  Our advice:  Don’t use and ammature toothbrush.   Use our toothbrush.  It has plenty of experience. ',
+            price: '₩29000 / $29.99',
             buy: 'BUY',
         },
     ]
@@ -77,10 +77,10 @@ const merch_string_builder = () => {
     let new_string = '';
     for(let i=0; i<merch_data.items.length; i++){
         new_string += `<div class="Items">`
-        new_string += `<h3>${merch_data.items[i].name}</h3>`
+        new_string += `<h2>${merch_data.items[i].name}</h2>`
         new_string += `<img src="${merch_data.items[i].picture}"`
         new_string += `<p>${merch_data.items[i].description}</p>`
-        new_string += `<h5>${merch_data.items[i].price}</p>`
+        new_string += `<h3>${merch_data.items[i].price}</h3>`
         new_string += `<button>${merch_data.items[i].buy}</button>`
     }
     printToDom(new_string, 'merch-store');
