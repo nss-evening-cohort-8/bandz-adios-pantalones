e6b9b02b6c68b8511fb55ecff4c1bb77ca8e3964
Wayne Collier <ke4tri@gmail.com>
2018-09-14 19:21:46 -0500

    added function and print to DOM for the favorite songs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 
 const printToDom = (stringToPrint, elementId) => {
-    const selectedDiv = document.getElementById(elementId);
-    selectedDiv.innerHTML = stringToPrint;
+  const selectedDiv = document.getElementById(elementId);
+  selectedDiv.innerHTML = stringToPrint;
 };
 
-const ourAlbums = [
+  const ourAlbums = [
     {
       name: 'My Boo',
-      songs: [{ id: 0, songTitle: "when I'm gone" }, { id: 1, songTitle: 'sugar' }, { id: 2, songTitle: 'tootsie' }]
+      songs: [
+        { id: 0, songTitle: "When I'm Gone" },
+        { id: 1, songTitle: 'Sugar' },
+        { id: 2, songTitle: 'Tootsie' }
+      ]
     },
     {
       name: 'Bingo',
-      songs: [{ id: 3, songTitle: 'hey baby' }, { id: 4, songTitle: 'kiss me twice' }, { id: 5, songTitle: 'cheating wife' }]
+      songs: [
+        { id: 3, songTitle: 'Hey Baby' },
+        { id: 4, songTitle: 'Kiss Me Twice' },
+        { id: 5, songTitle: 'Cheating Wife' }
+      ]
     }
   ];
   
@@ -22,15 +30,15 @@ const ourAlbums = [
     },
     {
       name: 'Robo',
-      favSongs: [{ id: 1 }, { id: 3 }, { id: 5 }]
+      favSongs: [{ id: 4 }, { id: 2 }, { id: 1 }]
     },
     {
       name: 'Chabo',
-      favSongs: [{ id: 1 }, { id: 3 }, { id: 5 }]
+      favSongs: [{ id: 2 }, { id: 3 }, { id: 0 }]
     },
     {
       name: 'Timbo',
-      favSongs: [{ id: 1 }, { id: 3 }, { id: 5 }]
+      favSongs: [{ id: 2 }, { id: 3 }, { id: 5 }]
     },
     {
       name: 'Waybo',
@@ -38,53 +46,125 @@ const ourAlbums = [
     }
   ];
   
-/// Media - Image Gallery ///
+  /// Media - Image Gallery ///
+  
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+    showDivs((slideIndex += n));
+  }
+  
+  function showDivs(n) {
+    var newImages = document.getElementsByClassName('media-images');
+    if (n > newImages.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = newImages.length;
+    }
+    for (i = 0; i < newImages.length; i++) {
+      newImages[i].style.display = 'none';
+    }
+    // BELOW IS WHAT I HAD TO COMMENT OUT FOR MY FUNCTION TO WORK ON 
+    // MY PAGE
+    // newImages[slideIndex - 1].style.display = 'block';
+  }
 
-// var slideIndex = 1;
-// showDivs(slideIndex);
+  /// End Media Image Gallery ///
 
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// };
-
-// function showDivs(n) {
-//   var newImages = document.getElementsByClassName("media-images");
-//   if ( n > newImages.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = newImages.length}
-//   for (i = 0; i < newImages.length; i++) {
-//      newImages[i].style.display = "none";  
-//   }
-//   newImages[slideIndex-1].style.display = "block";  
-// }
-
-/// End Media Image Gallery ///
-
-  function bandBioStringBuilder() {
-    let newString = '';
-    // Loop through the Bios
-    for (let i = 0; i < ourBios.length; i++) {
-      newString += `<h3>${ourBios[i].name}</h3>`;
-      // Loop through the array of favorite songs
-      for (let s = 0; s < ourBios[i].favSongs.length; s++) {
-        // Loop through the Albums and do a find for the SongTitle that matches the Favorte Song ID in the Bio.
-        for (let album = 0; album < ourAlbums.length; album++) {
-          // Grab the songId from the Band memeber Bio
-          let favSongId = ourBios[i].favSongs[s].id;
-          let songFindResult = ourAlbums[album].songs.find((songId) => songId.id === favSongId);
-          // If we don't find a song match in the Album it returns undefind so just move along.
-          if (songFindResult !== undefined) {
-            // We found a match so grab the song title from the result and put it in a <li>
-            newString += `<li>${songFindResult.songTitle}</li>`;
+  // Tour Schedule Array listing current tour dates
+  const tourScheduleDates = [
+    {
+      // Mon Sep 17 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1537160400000,
+      city: 'Pulaski',
+      state: 'TN',
+      venue: 'Pulaski Lions Club',
+      isSoldOut: 'true'
+    },
+    {
+      // Fri Aug 31 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1535691600000,
+      city: 'Scratch Ankle',
+      state: 'AL',
+      venue: "Jim's Waterin' Hole",
+      isSoldOut: 'false'
+    },
+    {
+      // Fri Aug 17 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1534482000000,
+      city: 'Flippin',
+      state: 'GA',
+      venue: 'Peach Pot',
+      isSoldOut: 'false'
+    },
+    {
+      // Sat Jul 28 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1532754000000,
+      city: 'Coward',
+      state: 'SC',
+      venue: "Surf n' Turf Karaoke Bar",
+      isSoldOut: 'true'
+    },
+    {
+      // Sat Jul 07 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1530939600000,
+      city: 'Whynot',
+      state: 'NC',
+      venue: '',
+      isSoldOut: 'false'
+    },
+    {
+      // Sun Jun 24 2018 00:00:00 GMT-0500 (Central Daylight Time)
+      date: 1529816400000,
+      city: 'Booger Hole',
+      state: 'WV',
+      venue: "You Pickem' We Serve 'em",
+      isSoldOut: 'false'
+    }
+  ];
+  
+  function bandBioStringBuilder(bio_name) {
+    newString = '';
+    for (let i=0;i<ourBios.length;i++){
+      if(ourBios[i]["name"] == bio_name) {
+        for (let s = 0; s < ourBios[i].favSongs.length; s++) {     
+          for (let album = 0; album < ourAlbums.length; album++) {
+            let favSongId = ourBios[i].favSongs[s].id;
+            let songFindResult = ourAlbums[album].songs.find(
+              (songId) => songId.id === favSongId);
+            if (songFindResult !== undefined) {
+              newString += `<li>${songFindResult.songTitle}</li>`;
+            }
           }
-       }
-     }
-      // Close up the <ul>
-     newString += `</ul>`;
-   }
-   printToDom(newString, "dom1");
+        }
+      }
+      // newString += `</ul>`;
+    }
+    newString = `<p>Favorite Songs` + `<ul>`+ newString + `</ul>`;
+    printToDom(newString, bio_name);
   };
-//   document.getElementById("dom1").addEventListener("click", function(){
-//     translate("spanish");
-// });
-bandBioStringBuilder('maco');
+// YES I AM GOING TO WORK THESE BELOW INTO ONE FUNCTION
+  document.getElementById("Maco").addEventListener("click", function(){
+    bandBioStringBuilder("Maco");
+  });
+  
+  document.getElementById("Robo").addEventListener("click", function(){
+    bandBioStringBuilder("Robo");
+  });
+
+  document.getElementById("Chabo").addEventListener("click", function(){
+    bandBioStringBuilder("Chabo");
+  });
+
+  document.getElementById("Timbo").addEventListener("click", function(){
+    bandBioStringBuilder("Timbo");
+  });
+
+  document.getElementById("waybo").addEventListener("click", function(){
+    bandBioStringBuilder("Waybo");
+  });
+
+
 
